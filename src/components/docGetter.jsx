cc7f85de1c5904fb7268b73d9c79e3b041ecd0a3
ADWAITH RAJ <117984453+adw-ith@users.noreply.github.com>
2023-09-04 21:20:05 +0530

    added: /src/components/docGetter.css

--- src/components/docGetter.jsx
+++ src/components/docGetter.jsx
@@ -1,43 +1,45 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
-function DocGetter(props)
-{
-  var dep,sub,mod,sem;
-  useEffect(()=>
-  {
-     sub=props.subject;
-     mod=props.module;
-     getDocs();
+import React, { useEffect } from "react";
+import axios from "axios";
+
+import "./docGetter.css";
+function DocGetter(props) {
+  var dep, sub, mod, sem;
+  useEffect(() => {
+    sub = props.subject;
+    mod = props.module;
+    getDocs();
   });
-   function getDocs()
-  {
+  function getDocs() {
     console.log(mod);
     console.log(sub);
-    document.getElementById('doclinks').innerHTML='';
-     axios.get('https://sheet.best/api/sheets/79c1bd6f-f522-42b2-99fe-2b7adc66c3a4').then(res=>{
-    const length=res.data.length;
-    for(var i=0;i<length;i++)
-    {
-      const details=res.data[i];
-      if(details.subject==sub&&details.module==mod)
-      {
-        const x=document.createElement('a');
-        x.style.textDecoration='none';
-        x.style.color='white';
-        var lin=details.file; 
-        x.setAttribute('href',lin);
-        x.innerHTML=lin;
-        document.getElementById('doclinks').appendChild(x);  
-      }
-    }
-  }); 
+    document.getElementById("doclinks").innerHTML = "";
+    axios
+      .get("https://sheet.best/api/sheets/79c1bd6f-f522-42b2-99fe-2b7adc66c3a4")
+      .then((res) => {
+        const length = res.data.length;
+        for (var i = 0; i < length; i++) {
+          const details = res.data[i];
+          if (details.subject == sub && details.module == mod) {
+            const x = document.createElement("a");
+            x.style.textDecoration = "none";
+            x.style.color = "white";
+            var lin = details.file;
+            x.setAttribute("href", lin);
+            x.setAttribute("class", "link");
+            x.innerHTML = "GET NOTES FOR " + mod + " OF " + sub;
+            document.getElementById("doclinks").appendChild(x);
+          }
+        }
+      });
   }
-    return(
-    <div className='linkContainer'>
-    <button onClick={props.closer}>close</button>
-     {/* <button className='sems'  onClick={getDocs}>get notes</button><br></br><br></br> */}
-      <div id='doclinks'></div>
+  return (
+    <div className="linkContainer">
+      {/* <button className='sems'  onClick={getDocs}>get notes</button><br></br><br></br> */}
+      <div id="doclinks" className="linkbox"></div>
+      <div onClick={props.closer} className="close-btn">
+        <img src="/close.svg" alt="" />
+      </div>
     </div>
-   )
+  );
 }
 export default DocGetter;
