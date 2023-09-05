dd807e8e610beeddb5165edfe7bf0eff5f65490b
Adwaith Raj <adwaithraj45654@gmail.com>
2023-09-06 00:04:27 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-
-
-import {Route,Routes} from 'react-router-dom'
-import docGetter from './components/docGetter'
-import './App.css'
-import Home from './Pages/home/home.jsx'
-import CS from './Pages/cs/cs'
-import DocumentUploader from './Pages/contribute/contribute'
+import { Route, Routes } from "react-router-dom";
+import docGetter from "./components/docGetter";
+import "./App.css";
+import Home from "./Pages/home/home.jsx";
+import CS from "./Pages/cs/cs";
+import EEE from "./Pages/EEE/EEE";
+import DocumentUploader from "./Pages/contribute/contribute";
 function App() {
-  
   return (
     <div>
-     
       <Routes>
-        <Route exact path='/' element={<Home/>}/>
-        <Route exact path='/cs' element={<CS/>}/>
-        <Route exact path='/contribute' element={<DocumentUploader/>}/>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/cs" element={<CS />} />
+        <Route exact path="/eee" element={<EEE />} />
+        <Route exact path="/contribute" element={<DocumentUploader />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
