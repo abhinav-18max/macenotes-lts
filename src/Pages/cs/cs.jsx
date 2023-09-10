b34e98b8d6f103de2366c959338a90768f1e9369
Adwaith Raj <adwaithraj45654@gmail.com>
2023-09-10 18:30:24 +0530

    small chngs

--- src/Pages/cs/cs.jsx
+++ src/Pages/cs/cs.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import Pagemodal from '../page_modal/pageModal';
-import data from '../data/cs.json';
-function CS()
-{
-    return(<><Pagemodal 
-    sem3={ data.sem3} sem4={data.sem4} sem5={data.sem5}  sem6={data.sem6} sem7={data.sem7} sem8={data.sem8}
-        heading='Computer Science'/></>);
+import React from "react";
+import Pagemodal from "../page_modal/pageModal";
+import data from "../data/sems.json";
+function CS() {
+  const sem = "semester 3";
+  return (
+    <>
+      <Pagemodal sem={data["cs"][sem]} heading="Subjects" />
+    </>
+  );
 }
 export default CS;
